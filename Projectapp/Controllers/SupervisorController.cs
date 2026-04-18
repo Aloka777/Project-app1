aeff8a54a41b01c27866d37e1a1cc653a0634f3c
venujaks <venujasamarasinghe2003@gmail.com>
2026-04-19 03:57:00 +0530

    "Added Supervisor Dashboard and Interests interfaces with Accept/Remove functionality"

---
+++ Projectapp/Controllers/SupervisorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projectapp.Controllers
+{
+    public class SupervisorController : Controller
+    {
+        
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult FilterProjects(string sortBy, string area, string type)
+        {
+            
+            return RedirectToAction("Dashboard");
+        }
+
+        
+        public IActionResult AddToInterests(int id)
+        {
+            
+            return RedirectToAction("Dashboard");
+        }
+
+        public IActionResult Interests()
+        {
+            return View();
+        }
+    }
+}
--- Projectapp/Program.cs
+++ Projectapp/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Student}/{action=Index}/{id?}");
+    pattern: "{controller=Supervisor}/{action=Index}/{id?}");
 
 app.Run();
